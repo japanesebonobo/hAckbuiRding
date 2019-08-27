26337559be84462e6caa8097471a7e3792d6bf3d
japanesebonobo <tomi119119@gmail.com>
2019-08-27 22:26:17 +0900

    Initial Commit

---
+++ picture on ARCard/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  picture on ARCard
+//
+//  Created by 吉冨優太 on 2019/08/27.
+//  Copyright © 2019 吉冨優太. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let defaultConfiguration: ARWorldTrackingConfiguration = {
+        let configuration = ARWorldTrackingConfiguration()
+        
+        let images = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
+        
+        configuration.detectionImages = images
+        configuration.planeDetection = .horizontal
+        configuration.environmentTexturing = .automatic
+        return configuration
+    }()
+    
+    var image = UIImage(named: "art.scnassets/sword.png")!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        sceneView.session.run(defaultConfiguration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+    
+    private func setImageToScene(image: UIImage) {
+        if let camera = sceneView.pointOfView {
+            let position = SCNVector3(x: 0, y: 0, z: -0.5) // 偏差のベクトルを生成する
+            let convertPosition = camera.convertPosition(position, to: nil)
+            let node = createPhotoNode(image, position: convertPosition)
+            self.sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
+    private func createPhotoNode(_ image: UIImage, position: SCNVector3) -> SCNNode {
+        let node = SCNNode()
+        let scale: CGFloat = 0.3
+        let geometry = SCNBox(width: image.size.width * scale / image.size.height,
+                              height: scale,
+                              length: 0.00000001,
+                              chamferRadius: 0.0)
+        geometry.firstMaterial?.diffuse.contents = image
+        node.geometry = geometry
+        node.position = position
+        return node
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else {
+            return
+        }
+        //名刺の上にボタンを表示
+        setImageToScene(image: image)
+    }
+}
