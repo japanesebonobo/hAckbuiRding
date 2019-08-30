7fc33b1d0557e485f44e3151becd31f45075c1e0
japanesebonobo <tomi119119@gmail.com>
2019-08-30 17:08:49 +0900

    レイヤー3の7Fまでオブジェクト以外実装

---
+++ picture on ARCard/FiveFloorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FiveFloorViewController.swift
+//  picture on ARCard
+//
+//  Created by 吉冨優太 on 2019/08/30.
+//  Copyright © 2019 吉冨優太. All rights reserved.
+//
+
+import UIKit
+
+class FiveFloorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ picture on ARCard/SixViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SixViewController.swift
+//  picture on ARCard
+//
+//  Created by 吉冨優太 on 2019/08/30.
+//  Copyright © 2019 吉冨優太. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class SixViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func StarbacksTapped(_ sender: UIButton) {
+        guard let url = URL(string: "https://loco.yahoo.co.jp/place/g-5yIasR6MdxM/?genrecd=01") else { return }
+        let safariController = SFSafariViewController(url: url)
+        present(safariController, animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
